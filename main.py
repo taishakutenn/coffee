3948492702468420e1eb8d987fd7b3ee8685fd0f
Taisha (artem) <sunlesslove128@gmail.com>
2024-12-12 13:28:35 +0500

    Новые фичи у нашего магазина кофе, сюдааа

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import sqlite3
 
 from PyQt6 import uic
-from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget
 
 
 class CoffeInformation(QMainWindow):
@@ -13,6 +13,7 @@ class CoffeInformation(QMainWindow):
     def initUI(self):
         uic.loadUi("main.ui", self)
         self.getCoffeeInfoButton.clicked.connect(self.get_coffee_information)
+        self.changeCoffeeInformationButton.clicked.connect(self.change_information)
 
     def get_coffee_information(self):
         self.showCoffeeInfoList.clear()
@@ -27,6 +28,50 @@ class CoffeInformation(QMainWindow):
         for i in response:
             self.showCoffeeInfoList.addItem(f"id: {i[0]}\nНазвание сорта: {i[1]}\nСтепень обжарки: {i[2]}"
                                             f"\nМолотый или нет: {i[3]}\nОписание вкуса: {i[4]}\nЦена: {i[5]} руб. за {i[6]} г.\n")
+
+    def change_information(self):
+        self.new_win = ChangingCoffeeInformation()
+        self.new_win.show()
+
+
+class ChangingCoffeeInformation(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.createNewCoffeInfoButton.clicked.connect(self.create_new_info)
+        self.changeCoffeeInfoButton.clicked.connect(self.change_info)
+
+    def create_new_info(self):
+        info = (self.nameSortEdit.text(), self.objarkaEdit.text(), self.molotiOrZernaButton.text(), self.descriptionButton.text(), self.priceEdit.text(), self.volumeButton.text())
+        conn = sqlite3.connect("coffee.sqlite")
+        cur = conn.cursor()
+        sql = '''INSERT INTO coffe(variety_name, degree_doneness, ground_or_grains, description_taste, price, packaging_volume)
+                 VALUES (?, ?, ?, ?, ?, ?)'''
+        cur.execute(sql, info)
+        conn.commit()
+        conn.close()
+        self.close()
+
+    def change_info(self):
+        info = (self.nameSortEdit.text(), self.objarkaEdit.text(), self.molotiOrZernaButton.text(),
+                self.descriptionButton.text(), self.priceEdit.text(), self.volumeButton.text(), self.idEdit.text())
+        conn = sqlite3.connect("coffee.sqlite")
+        cur = conn.cursor()
+        sql = '''UPDATE coffe
+                 SET variety_name = ?,
+                 degree_doneness = ?,
+                 ground_or_grains = ?, 
+                 description_taste = ?,
+                 price = ?,
+                 packaging_volume = ?
+                 WHERE id = ?'''
+        cur.execute(sql, info)
+        conn.commit()
+        conn.close()
+        self.close()
 
 
 if __name__ == "__main__":
